8a456056bb04d84b0a46476043490bddc4681db4
onion-debugger <oshiole94@gmail.com>
2022-09-02 16:52:50 +0100

    Navigation and screen set-up

--- app/src/main/java/com/example/pasteltakehome/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pasteltakehome/presentation/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph
 import com.example.pasteltakehome.ui.theme.PastelTakeHomeTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +28,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold() {
+                        DestinationsNavHost(navGraph = NavGraphs.root)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PastelTakeHomeTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/pasteltakehome/ui/theme/Color.kt
+++ app/src/main/java/com/example/pasteltakehome/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package com.example.pasteltakehome.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val Dark = Color(0xFF222325)
+val TextWhite = Color(0xFFFFFFFF)
+val Cream = Color(0xFFD9DDDC)
+val Grey = Color(0xFF2B2C2C)
